2c3dac5d33f1bfb8fd385d14616cf81c6f8949cf
jaredoyuela <145075881+jaredoyuela@users.noreply.github.com>
2024-04-29 19:37:35 -0400

    Modifed 03-happy.sh, did the exercise

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,11 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+echo "Is today the weekend?"
+day=$(date +%u)  # Get the day of the week (1-7, where 6 and 7 are weekend days)
+
+if [ $day -eq 6 ] || [ $day -eq 7 ]; then
+    echo "Today is the weekend!"
+else
+    echo "Today is not the weekend."
+fi
